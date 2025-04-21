bb5a3027e074ed52a41d69ae3d93dc81a7ddbd95
DM140102 <diogo.mendes02@gmail.com>
2025-04-21 20:25:08 +0200

    Fix Vite build config to resolve index.html

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // explicitly set the project root
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html',
+    },
+  },
 });
